c6fbd3a4c69c6e9f60d570c7e8f3a132a2a5f87c
orkunasa@gmail.com <orkunasa@gmail.com>
2017-07-07 18:26:30 +0300

    - !!! Migrate !!! - Some properties add to Arcade - Some improvements done in campaign page

--- arcade/models.py
+++ arcade/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Machine(models.Model):
     name = models.CharField(max_length=255)
     money = models.IntegerField(default=0)
+    status = models.BooleanField(default=0)
+    start_date = models.DateField(blank=True,auto_now=False)
+    end_date = models.DateField(null=True,blank=True,auto_now=False, auto_now_add=False)
+    location_name = models.CharField(null=True,max_length=255)
+    loc_pos_lat = models.FloatField(default=41.068178)
+    loc_pos_lon = models.FloatField(default=28.946577)
 
     def __str__(self):
         return self.name
