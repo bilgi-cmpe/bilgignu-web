5f4b0823ea96cbbdd1f41fef4d9853c272299dd5
Enis Behiç Tuysuz <enisbtuysuz@gmail.com>
2017-09-27 19:31:32 +0300

    Modified update money view

--- arcade/views.py
+++ arcade/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import UpdateAPIView, ListAPIView
+from rest_framework.response import Response
 
 from .serializers import MachineSerializer
 from .models import Machine
@@ -22,6 +23,18 @@ class MachineUpdateView(UpdateAPIView):
 
     def get_queryset(self):
         return Machine.objects.filter(id=self.kwargs["pk"])
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        instance.money += int(request.data.get("money"))
+        instance.save()
+
+        serializer = self.get_serializer(instance, data={}, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
 
 
 class ArcadeView(View):
